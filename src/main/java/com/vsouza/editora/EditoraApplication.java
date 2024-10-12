6005fece2ef54c2a467847679917c5d6277887f0
veronica.souza <veronica.souza@icts.com.br>
2024-10-12 13:00:06 -0300

    initial commit

---
+++ src/main/java/com/vsouza/editora/EditoraApplication.java
@@ -0,0 +1,13 @@
+package com.vsouza.editora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsouza/editora/EditoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsouza.editora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
