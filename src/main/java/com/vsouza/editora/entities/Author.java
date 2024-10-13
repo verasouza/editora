15022f3cf4098ff1a7e5ed817832150ba198e813
veronica.souza <veronica.souza@icts.com.br>
2024-10-13 17:36:21 -0300

    ajuste apara aparecer a página do swagger

--- src/main/java/com/vsouza/editora/entities/Author.java
+++ src/main/java/com/vsouza/editora/entities/Author.java
@@ -1,6 +1,7 @@
 package com.vsouza.editora.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,7 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fullName;
+    @Email
     private String email;
 
 
--- src/main/java/com/vsouza/editora/repositories/BookRepository.java
+++ src/main/java/com/vsouza/editora/repositories/BookRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByAuthorId(Long id);
+
 }
